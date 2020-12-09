404698f0538f4d8dd1fc7c7132ed5edecc68de03
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-12-08 21:45:31 -0500

    Add files via upload

--- Computer.java
+++ Computer.java
@@ -1,2 +1,23 @@
 public class Computer {
+    private Keyboard keyboard;
+    private Monitor monitor;
+    private Mouse mouse;
+
+    public Computer(Keyboard keyboard, Monitor monitor, Mouse mouse){
+        this.keyboard = keyboard;
+        this.monitor = monitor;
+        this.mouse = mouse;
+    }
+
+    public Keyboard getKeyboard() {
+        return keyboard;
+    }
+
+    public Monitor getMonitor() {
+        return monitor;
+    }
+
+    public Mouse getMouse() {
+        return mouse;
+    }
 }
